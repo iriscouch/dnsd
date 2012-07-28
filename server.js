38a3cd01427310abcf494d0964388b46fb4f0a4c
Jason Smith (work) <jhs@iriscouch.com>
2012-07-28 08:59:48 +0000

    The basic server idea

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ function Server (handler) {
   self.udp.on('error', function(er) { self.emit('error', er) })
   self.tcp.on('error', function(er) { self.emit('error', er) })
 
-  self.tcp.on('connection', function(connection) { self.on_connection(connection) })
-  self.udp.on('message', function(msg, rinfo) { self.on_message(msg, rinfo) })
+  self.tcp.on('connection', function(connection) { self.on_tcp_connection(connection) })
+  self.udp.on('message', function(msg, rinfo) { self.on_udp(msg, rinfo) })
 
   var listening = {'tcp':false, 'udp':false}
   self.udp.once('listening', function() {
@@ -72,16 +72,84 @@ Server.prototype.close = function() {
     })
 }
 
-Server.prototype.on_connection = function(connection) {
+Server.prototype.on_tcp_connection = function(connection) {
   var self = this
 
+  var length = null
+    , bufs = []
+
   connection.on('data', function(data) {
-    self.log.log('Data: %s', util.inspect(data))
+    bufs.push(data)
+    var bytes_received = bufs.reduce(function(state, buf) { return state + buf.length }, 0)
+
+    if(length === null && bytes_received >= 2) {
+      var so_far = Buffer.concat(bufs) // Flatten them all together, it's probably not much data.
+      length = so_far.readUInt16BE(0)
+      bufs = [ so_far.slice(2) ]
+    }
+
+    if(length !== null && bytes_received == 2 + length) {
+      // All of the data (plus the 2-byte length prefix) is received.
+      var data = Buffer.concat(bufs)
+        , req = new Message(data)
+        , res = new Response(data, {'socket':connection, 'type':'tcp'})
+
+      self.emit('request', req, res)
+    }
   })
 }
 
-Server.prototype.on_message = function(msg, rinfo) {
+Server.prototype.on_udp = function(data, rinfo) {
   var self = this
 
-  self.log.info('Message from %j: %s', rinfo, util.inspect(msg))
+  rinfo.socket = this.udp
+  rinfo.type = 'udp'
+
+  var req = new Message(data)
+    , res = new Response(data, rinfo)
+
+  self.emit('request', req, res)
+}
+
+
+util.inherits(Response, Message)
+function Response (data, peer) {
+  var self = this
+  Message.call(self, data)
+
+  self.question   = self.question   || []
+  self.answer     = self.answer     || []
+  self.authority  = self.authority  || []
+  self.additional = self.additional || []
+
+  // Instead of `self.peer = peer` use this to keep it out of the enumeration.
+  Object.defineProperty(self, 'peer', {'value':peer, 'enumerable':false, 'writable':true, 'configurable':true })
+}
+
+Response.prototype.end = function() {
+  var self = this
+  var data = require('fs').readFileSync(__dirname+'/test_data/oreilly.com-response')
+
+  if(self.peer.type == 'udp' && data.length > 512)
+    return self.emit('error', 'UDP responses greater than 512 bytes not yet implemented')
+
+  else if(self.peer.type == 'udp')
+    self.peer.socket.send(data, 0, data.length, self.peer.port, self.peer.address, function(er) {
+      if(er)
+        self.emit('error', er)
+    })
+
+  else if(self.peer.type == 'tcp') {
+    // Add the data length prefix.
+    var length = data.length
+    data = Buffer.concat([ new Buffer([length >> 8, length & 255]), data ])
+
+    self.peer.socket.end(data, function(er) {
+      if(er)
+        self.emit('error', er)
+    })
+  }
+
+  else
+    self.emit('error', new Error('Unknown peer type: ' + self.peer.type))
 }
