7a31f16111c34eee44aeceac9f04743e524f7cfe
Jason Smith (work) <jhs@iriscouch.com>
2012-07-28 08:59:48 +0000

    The basic server idea

--- test/server.js
+++ test/server.js
@@ -2,11 +2,15 @@
 //
 // Test DNS server
 
+var fs = require('fs')
 var tap = require('tap')
 var test = tap.test
 var util = require('util')
+var dgram = require('dgram')
 
 var API = require('../named')
+
+var PORT = 5321
 
 test('Server API', function(t) {
   // The idea is to mimic the http or net server API.
@@ -22,8 +26,6 @@ test('Server API', function(t) {
 })
 
 test('Network server', function(t) {
-  var port = 5321
-
   var server = API.createServer(function(req, res) {
     console.log('Req: %j', req)
     console.log('Res: %j', res)
@@ -35,7 +37,7 @@ test('Network server', function(t) {
   server.on('close', function() { events.close += 1 })
   server.on('error', function() { events.error += 1 })
 
-  server.listen(port, '127.0.0.1')
+  server.listen(PORT, '127.0.0.1')
   setTimeout(check_init, 150)
   setTimeout(check_stop, 200)
 
@@ -53,4 +55,46 @@ test('Network server', function(t) {
 
     t.end()
   }
+})
+
+test('UDP queries', function(t) {
+  var reqs = { 'ru.ac.th'                   : { id:36215, opcode:'update', recursion:false, name:'dynamic-update' }
+             , 'oreilly.com'                : { id:45753, opcode:'query' , recursion:true , name:'oreilly.com-query' }
+             , 'www.company.example'        : { id:62187, opcode:'query' , recursion:true , name:'www.company.example-query' }
+             , 'www.microsoft.com.nsatc.net': { id:47096, opcode:'query' , recursion:true , name:'www.microsoft.com-query' }
+             }
+
+  var i = 0
+  var server = API.createServer(check_req)
+  server.listen(PORT, '127.0.0.1')
+
+  function check_req(req, res) {
+    t.type(req.question, 'Array', 'Got a question message')
+    t.equal(req.question.length, 1, 'Got exactly one question')
+
+    var question = req.question[0]
+      , name = question.name
+      , expected = reqs[name]
+
+    t.ok(expected, 'Found expected request: ' + name)
+    t.equal(req.id, expected.id, 'ID match: ' + name)
+    t.equal(req.opcode, expected.opcode, 'Opcode match: ' + name)
+    t.equal(req.recursion_desired, expected.recursion, 'Recursion match: ' + name)
+
+    res.end()
+
+    i += 1
+    if(i == 4) {
+      server.close()
+      t.end()
+    }
+  }
+
+  Object.keys(reqs).forEach(function(domain) {
+    var data = fs.readFileSync(__dirname + '/../test_data/' + reqs[domain].name)
+    //sent.push(data)
+
+    var sock = dgram.createSocket('udp4')
+    sock.send(data, 0, data.length, PORT, '127.0.0.1', function() { sock.close() })
+  })
 })
