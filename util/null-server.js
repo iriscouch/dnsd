a93eae9c1445d4c699c1f7958bb463ed238fada4
Jason Smith (work) <jhs@iriscouch.com>
2012-07-29 15:54:11 +0000

    Use res.end() to send a dynamic IP address

--- util/null-server.js
+++ util/null-server.js
@@ -5,7 +5,7 @@ var named = require('../iris-named')
 var port = +(process.argv[2] || 5321)
 named.createServer(function(req, res) {
   console.log('%s:%s/%s %j', req.connection.remoteAddress, req.connection.remotePort, req.connection.type, req)
-  res.end()
+  res.end('1.1.1.' + req.question[0].name.length)
 }).listen(port)
   .zone('oreilly.com', 'ns1.iriscouch.net', 'us@iriscouch.com', 'now', 7200, 1800, 1209600, 600)
 
